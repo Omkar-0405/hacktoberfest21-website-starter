3792dd655a6d89cfe02601cd084dabaf9e2960d3
krish10k <krushna.dahake.01@gmail.com>
2021-11-01 04:24:34 +0530

    feat:pointer interaction

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ParallaxProvider } from "react-scroll-parallax";
+import MouseParticles from "react-mouse-particles";
 ReactDOM.render(
   <ParallaxProvider>
-    <App />
+    <div>
+      <MouseParticles
+        g={0.3}
+        num={9}
+        radius={8}
+        life={0.8}
+        v={0.8}
+        color="black"
+        alpha={0.26}
+        level={6}
+      />
+      <App />
+    </div>
   </ParallaxProvider>,
   document.getElementById("root")
 );
